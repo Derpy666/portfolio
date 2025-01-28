9dae462ba7808b86790ef09501db5db00d345776
Derpy666 <noamgl2002@gmail.com>
2025-01-28 15:55:44 +0200

    changed the contact form submit

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const form = document.getElementById('contact');
+const formContact = document.getElementById('contact');
 const modal = new bootstrap.Modal(document.getElementById('successModal'));
 
-form.addEventListener('submit', function (event) {
+formContact.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const form = event.target;
     if (form.checkValidity()) {
-        event.target[0].value = ""
-        event.target[1].value = ""
-        event.target[2].value = ""
-      modal.show();
+        form.reset();
+        modal.show();
     } else {
       form.reportValidity();
     }
